47e60ccfbe5a94cee28864a10854429d59b30105
Djordje <djordjeerkicecka@users.noreply.github.com>
2023-05-31 11:28:53 +0200

    Production subtree

---
+++ js/scripts.js
@@ -0,0 +1,131 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 365:
+/***/ (() => {
+
+const jsBody = document.querySelector('.js-body');
+const jsHeader = document.querySelector('.js-header');
+const jsNav = document.querySelector('.js-nav');
+const jsNavToggler = document.querySelector('.js-nav-toggler');
+
+
+jsNavToggler.addEventListener('click', e => {
+  const state = e.currentTarget.getAttribute('aria-expanded') === 'true';
+  e.currentTarget.setAttribute('aria-expanded', !state);
+
+  if (state) {
+    // open -> close
+    jsBody.setAttribute('data-scroll', 'scroll');
+    jsNav.setAttribute('data-state', 'closing');
+    jsNav.addEventListener('animationend', e => {
+        e.currentTarget.setAttribute('data-state', 'closed');
+        jsHeader.setAttribute('aria-expanded', false)
+    }, {once: true});
+  } else {
+    // close -> open
+    jsBody.setAttribute('data-scroll', 'no-scroll');
+    jsHeader.setAttribute('aria-expanded', true)
+    jsNav.setAttribute('data-state', 'open');
+  }
+});
+
+
+// Observe About To Trigger Navigation Styles;
+const jsNavObserverTrigger = document.querySelector('.js-nav-observer-trigger');
+const observerConfig = {
+  root: null,
+  threshold: 0.3,
+  rootMargin: '0px',
+};
+
+const navbarObserver = new IntersectionObserver((entries) => {
+  const [entry] = entries;
+
+  if(entry.isIntersecting) {
+    jsHeader.classList.add('is-scrolling');
+  }else {
+    jsHeader.classList.remove('is-scrolling');
+  }
+
+}, observerConfig);
+
+navbarObserver.observe(jsNavObserverTrigger)
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/* harmony import */ var _navigation_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(365);
+/* harmony import */ var _navigation_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_navigation_js__WEBPACK_IMPORTED_MODULE_0__);
+
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
